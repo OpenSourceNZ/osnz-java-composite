9c7929183942a2c87395af32ca742ed65bb2373a
Kefeng Deng <deng@51any.com>
2020-11-17 08:53:01 +1300

    added sample and github actions

---
+++ samples/spring-boot-webflux-netty-sample/src/main/java/io/osnz/Application.java
@@ -0,0 +1,33 @@
+package io.osnz;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Kefeng Deng (deng@51any.com)
+ */
+@SpringBootApplication
+@RestController
+@EnableWebFlux
+@Log4j2
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(Application.class);
+    app.setBannerMode(Banner.Mode.OFF);
+    app.run(args);
+    log.info("Server is ready ...");
+  }
+
+  @GetMapping(path = "/")
+  public Mono<String> index() {
+    return Mono.just("Hello World");
+  }
+
+}
